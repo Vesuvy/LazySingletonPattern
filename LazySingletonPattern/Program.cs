d66b224d90570b76c4fb2f2bfc8ab8233b40ddf6
VesuvyX <antonbaenkov7@gmail.com>
2024-05-14 23:47:53 +0700

    Добавьте файлы проекта.

---
+++ LazySingletonPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LazySingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // добавляем сервера
+            Console.WriteLine("Добавляю НЕверный сервер " + Servers.Instance.AddServer("ht://example.ru"));
+            Console.WriteLine("Добавляю Верный 1 сервер http " + Servers.Instance.AddServer("http://example.ru"));
+            Console.WriteLine("Добавляю Верный 2 сервер http " + Servers.Instance.AddServer("http://example2.ru"));
+            Console.WriteLine("Добавляю Верный 1 сервер https " + Servers.Instance.AddServer("https://example.ru"));
+            Console.WriteLine("Добавляю Верный 2 сервер https " + Servers.Instance.AddServer("https://example2.ru"));
+            Console.WriteLine("Добавляю Верный 2 сервер https, но повторно " + Servers.Instance.AddServer("https://example2.ru"));
+
+            // проверяем http
+            Console.WriteLine("http сервера:");
+            foreach (var server in Servers.Instance.GetHttpServers())
+            {
+                Console.WriteLine(server);
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            // проверяем  https
+            Console.WriteLine("https сервера:");
+            foreach (var server in Servers.Instance.GetHttpsServers())
+            {
+                Console.WriteLine(server);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LazySingletonPattern/ServersSingleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LazySingletonPattern
+{
+    public class Servers
+    {
+        private static readonly Lazy<Servers> _instance = new Lazy<Servers>(() => new Servers());
+        private readonly List<string> _serversList = new List<string>();
+        private static object syncRoot = new object();
+
+        private Servers() {
+            _serversList = new List<string>();
+        }
+
+        public static Servers Instance => _instance.Value;
+
+        public bool AddServer(string server)
+        {
+            lock (syncRoot)
+            {
+                if (server == "" || !server.StartsWith("http") && !server.StartsWith("https"))
+                {
+                    Console.WriteLine("неверный формат");
+                    return false;
+                }
+                    
+                else
+                {
+                    if (_serversList.Contains(server))
+                        return false;
+                    else
+                    {
+                        _serversList.Add(server);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<string> GetHttpServers()
+        {
+            lock (syncRoot)
+                return _serversList.Where(server => server.StartsWith("http:"));
+        }
+
+        public IEnumerable<string> GetHttpsServers()
+        {
+           lock (syncRoot)
+                return _serversList.Where(server => server.StartsWith("https:"));
+        }
+    }
+}
